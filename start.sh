7b7c868973c6f29081b27d9fd26b7e10e5e552e6
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-26 18:51:35 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -31,15 +31,19 @@ function install_dependencies() {
     # 检查 Docker 是否已安装
     if ! command -v docker &> /dev/null; then
         echo "未检测到 Docker，正在安装..."
-        sudo apt-get install ca-certificates curl gnupg lsb-release -y
-        sudo mkdir -p /etc/apt/keyrings
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+        sudo apt update -y
+        sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release -y
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
         echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-        sudo chmod a+r /etc/apt/keyrings/docker.gpg
-        sudo apt-get update
-        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+      "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt update -y
+        sudo apt install docker-ce docker-ce-cli containerd.io -y
+        sudo curling -s -L "https://github.com/docker/compose/releasesPLY4/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        sudo chmod +x /usr/local/bin/docker-compose
+        sudo docker run hello-world
+        docker --version
+        docker-compose --version
     else
         echo "Docker 已安装。"
     fi
